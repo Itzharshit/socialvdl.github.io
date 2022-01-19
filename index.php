92d8a26a0995f3c1b22b2941289916f2bbb9b759
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-01-19 11:03:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ include("config.php");
 <div class="w3-input w3-border w3-round-large" >
 <a href="/"><center> <h1 > Socal DL </h1> </center></a> </div>
 <form action="searchfilter.php" class="w3-container w3-card-4 w3-light-grey">
-   <center> <h2>Enter video Link or search YouTube</h2> </center>
+   <center> <h2>Enter video Link or Search YouTube</h2> </center>
   <p>
   <input required="" class="w3-input w3-border w3-round-large" name="vkr" type="text"></p>
   <center>
